1fbcbe9d32b91aedc72ab80208358c6bedc357f4
Hieu <namno383@gmail.com>
2025-10-28 20:41:42 +0700

    update steps

--- Heathy_App_Backend/src/main/java/com/iuh/heathy_app_backend/dto/AuthResponse.java
+++ Heathy_App_Backend/src/main/java/com/iuh/heathy_app_backend/dto/AuthResponse.java
@@ -11,13 +11,13 @@ public class AuthResponse {
     private String token;
     private String type = "Bearer";
     private Long id;
-    private String username;
     private String email;
+    private String name;
 
-    public AuthResponse(String accessToken, Long id, String username, String email) {
+    public AuthResponse(String accessToken, Long id, String email, String name) {
         this.token = accessToken;
         this.id = id;
-        this.username = username;
         this.email = email;
+        this.name = name;
     }
 }
